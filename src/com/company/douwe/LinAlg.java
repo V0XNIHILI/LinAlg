c78038d23ebb476096bdf73f86aa20d6d7311a6f
Douwe den Blanken <douwedb@gmail.com>
2020-06-01 01:31:38 +0200

    Converted library to using 1D arrays only

--- src/com/company/douwe/LinAlg.java
+++ src/com/company/douwe/LinAlg.java
@@ -1,24 +1,36 @@
-//package com.company.douwe;
-//
-//import com.company.douwe.Matrix;
-//
-//public class LinAlg {
-//    /**
-//     * Multiply two matrices
-//     *
-//     * @param a First matrix
-//     * @param b Second matrix
-//     * @return
-//     * @throws IllegalArgumentException
-//     */
-//    public static Matrix multiply(Matrix a, Matrix b) throws IllegalArgumentException {
-//        if (a.height != b.width || a.width != b.height)
-//            throw new IllegalArgumentException("The dimensions of the matrices to be multiplied do not match");
-//
-//        double[][] result = new double[a.height][b.width];
-//        double[][] inputValuesA = a.getValues();
-//        double[][] inputValuesB = b.getValues();
-//
+package com.company.douwe;
+
+import com.company.douwe.Matrix;
+
+public class LinAlg {
+    /**
+     * Multiply two matrices
+     *
+     * @param a First matrix
+     * @param b Second matrix
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public static Matrix multiply(Matrix a, Matrix b) throws IllegalArgumentException {
+        if (a.height != b.width || a.width != b.height)
+            throw new IllegalArgumentException("The dimensions of the matrices to be multiplied do not match");
+
+        double[] result = new double[b.width * a.height];
+        double[] inputValuesA = a.getValues();
+        double[] inputValuesB = b.getValues();
+
+        for (int i = 0; i < b.width; i++) {
+            for (int j = 0; j < a.height; j++) {
+                double total = 0;
+
+                for (int k = 0; k < b.height; k++) {
+                    total += inputValuesA[j*a.width + k] * inputValuesB[k*b.width + i];
+                }
+
+                result[j * b.width + i] = total;
+            }
+        }
+
 //        for (int i = 0; i < b.width; i++) {
 //            for (int j = 0; j < a.height; j++) {
 //                double total = 0;
@@ -30,92 +42,88 @@
 //                result[j][i] = total;
 //            }
 //        }
-//
-//        return new Matrix(result);
-//    }
-//
-//    // @todo
-//    // Implement the divide function -> a * b^-1
-//
-//    /**
-//     * Add two matrices
-//     *
-//     * @param a First matrix
-//     * @param b Second matrix
-//     * @return
-//     * @throws IllegalArgumentException
-//     */
-//    public static Matrix add(Matrix a, Matrix b) throws IllegalArgumentException {
-//        if (b.width != a.width || b.height != a.height)
-//            throw new IllegalArgumentException("The dimensions of the matrices to be added do not match");
-//
-//        double[][] result = new double[a.height][a.width];
-//
-//        double[][] inputValuesA = a.getValues();
-//        double[][] inputValuesB = b.getValues();
-//
-//        for (int i = 0; i < a.width; i++) {
-//            for (int j = 0; j < a.height; j++) {
-//                result[j][i] = inputValuesA[j][i] + inputValuesB[j][i];
-//            }
-//        }
-//
-//        return new Matrix(result);
-//    }
-//
-//    /**
-//     * Subtract two matrices
-//     *
-//     * @param a First matrix (matrix to be subtracted from)
-//     * @param b Second matrix (matrix to subtract with)
-//     * @return
-//     * @throws IllegalArgumentException
-//     */
-//    public static Matrix subtract(Matrix a, Matrix b) throws IllegalArgumentException {
-//        if (b.width != a.width || b.height != a.height)
-//            throw new IllegalArgumentException("The dimensions of the matrices to be added do not match");
-//
-//        double[][] result = new double[a.height][a.width];
-//
-//        double[][] inputValuesA = a.getValues();
-//        double[][] inputValuesB = b.getValues();
-//
-//        for (int i = 0; i < a.width; i++) {
-//            for (int j = 0; j < a.height; j++) {
-//                result[j][i] = inputValuesA[j][i] - inputValuesB[j][i];
-//            }
-//        }
-//
-//        return new Matrix(result);
-//    }
-//
-//    /**
-//     * Apply a 'to-the-power' operation to a matrix
-//     *
-//     * @param a     The matrix where the power is computed from
-//     * @param power Power number
-//     * @return
-//     * @throws IllegalArgumentException
-//     */
-//    public static Matrix pow(Matrix a, int power) throws IllegalArgumentException {
-//        if (a.width != a.height)
-//            throw new IllegalArgumentException("The matrix is not square");
-//
-//        if (power < 0)
-//            throw new IllegalArgumentException("Power should be larger than 0");
-//
-//        // @todo
-//        // if power == 1, return the identity matrix
-//
-//        Matrix result = new Matrix(a.getValues());
-//
-//        for (int i = 1; i < power; i++) {
-//            result = LinAlg.multiply(result, a);
-//        }
-//
-//        return result;
-//    }
-//
+
+        return new Matrix(result, b.width, a.height);
+    }
+
+    // @todo
+    // Implement the divide function -> a * b^-1
+
+    /**
+     * Add two matrices
+     *
+     * @param a First matrix
+     * @param b Second matrix
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public static Matrix add(Matrix a, Matrix b) throws IllegalArgumentException {
+        if (b.width != a.width || b.height != a.height)
+            throw new IllegalArgumentException("The dimensions of the matrices to be added do not match");
+
+        double[] result = new double[a.width * a.height];
+
+        double[] inputValuesA = a.getValues();
+        double[] inputValuesB = b.getValues();
+
+        for (int i = 0; i < a.width * a.height; i++) {
+            result[i] = inputValuesA[i] + inputValuesB[i];
+        }
+
+        return new Matrix(result, a.width, a.height);
+    }
+
+    /**
+     * Subtract two matrices
+     *
+     * @param a First matrix (matrix to be subtracted from)
+     * @param b Second matrix (matrix to subtract with)
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public static Matrix subtract(Matrix a, Matrix b) throws IllegalArgumentException {
+        if (b.width != a.width || b.height != a.height)
+            throw new IllegalArgumentException("The dimensions of the matrices to be added do not match");
+
+        double[] result = new double[a.width * a.height];
+
+        double[] inputValuesA = a.getValues();
+        double[] inputValuesB = b.getValues();
+
+        for (int i = 0; i < a.width * a.height; i++) {
+            result[i] = inputValuesA[i] - inputValuesB[i];
+        }
+
+        return new Matrix(result, a.width, a.height);
+    }
+
+    /**
+     * Apply a 'to-the-power' operation to a matrix
+     *
+     * @param a     The matrix where the power is computed from
+     * @param power Power number
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public static Matrix pow(Matrix a, int power) throws IllegalArgumentException {
+        if (a.width != a.height)
+            throw new IllegalArgumentException("The matrix is not square");
+
+        if (power < 0)
+            throw new IllegalArgumentException("Power should be larger than 0");
+
+        // @todo
+        // if power == 1, return the identity matrix
+
+        Matrix result = new Matrix(a.getValues(), a.width, a.height);
+
+        for (int i = 1; i < power; i++) {
+            result = LinAlg.multiply(result, a);
+        }
+
+        return result;
+    }
+
 //    public static Matrix flipOverDiagonal(Matrix a) {
 //        double[][] result = new double[a.width][a.height];
 //        double[][] inputValues = a.getValues();
@@ -128,30 +136,28 @@
 //
 //        return new Matrix(result);
 //    }
-//
-//    /**
-//     * Perform elementwise matrix multiplication. For a mathematical reference, please see:
-//     * https://en.wikipedia.org/wiki/Hadamard_product_(matrices)
-//     *
-//     * @param b Matrix that you want to perform elementwise multiplication with
-//     * @return Result of the calculation
-//     * @throws IllegalArgumentException
-//     */
-//    public static Matrix hadamard(Matrix a, Matrix b) throws IllegalArgumentException {
-//        if (b.width != a.width || b.height != a.height)
-//            throw new IllegalArgumentException("The width and height of the matrices to be multiplied do not match");
-//
-//        double[][] result = new double[a.height][a.width];
-//
-//        double[][] inputValuesA = a.getValues();
-//        double[][] inputValuesB = b.getValues();
-//
-//        for (int i = 0; i < a.width; i++) {
-//            for (int j = 0; j < a.height; j++) {
-//                result[j][i] = inputValuesB[j][i] * inputValuesA[j][i];
-//            }
-//        }
-//
-//        return new Matrix(result);
-//    }
-//}
+
+    /**
+     * Perform elementwise matrix multiplication. For a mathematical reference, please see:
+     * https://en.wikipedia.org/wiki/Hadamard_product_(matrices)
+     *
+     * @param b Matrix that you want to perform elementwise multiplication with
+     * @return Result of the calculation
+     * @throws IllegalArgumentException
+     */
+    public static Matrix hadamard(Matrix a, Matrix b) throws IllegalArgumentException {
+        if (b.width != a.width || b.height != a.height)
+            throw new IllegalArgumentException("The width and height of the matrices to be multiplied do not match");
+
+        double[] result = new double[a.width * a.height];
+
+        double[] inputValuesA = a.getValues();
+        double[] inputValuesB = b.getValues();
+
+        for (int i = 0; i < a.width * a.height; i++) {
+            result[i] = inputValuesB[i] * inputValuesA[i];
+        }
+
+        return new Matrix(result, a.width, a.height);
+    }
+}
--- src/com/company/douwe/Main.java
+++ src/com/company/douwe/Main.java
@@ -17,7 +17,10 @@ public class Main {
         });
 
         // Show functionality of converting 1D double[] to matrix with given dimensions
-        Matrix h = new Matrix(new double[]{1, -2, 3, 4, 5, 6}, 3, 2);
+        Matrix h = new Matrix(new double[]{1, 2, 3, 4, 5, 6}, 3, 2);
+        Matrix m = new Matrix(new double[]{7, 8, 9, 10, 11, 12}, 2, 3);
+
+        System.out.println(LinAlg.multiply(h, m));
 
         Matrix g = new Matrix(5, 5, -2, 1);
 
@@ -26,20 +29,20 @@ public class Main {
 
         int size = 1;
 
-//        // Run matrix multiplication operations for matrix with size 1, 2, 4, 8 ... 512, 1024 and time them
-//        while (size < 2048) {
-//            Matrix k = new Matrix(size, size, -1, 1);
-//
-//            long startTime = System.currentTimeMillis();
-//
-//            Matrix l = LinAlg.pow(k, 2);
-//
-//            long endTime = System.currentTimeMillis();
-//
-//            System.out.println((endTime - startTime));
-//
-//            size *= 2;
-//        }
+        // Run matrix multiplication operations for matrix with size 1, 2, 4, 8 ... 512, 1024 and time them
+        while (size < 2048) {
+            Matrix k = new Matrix(size, size, -1, 1);
+
+            long startTime = System.currentTimeMillis();
+
+            Matrix l = LinAlg.pow(k, 2);
+
+            long endTime = System.currentTimeMillis();
+
+            System.out.println((endTime - startTime));
+
+            size *= 2;
+        }
 
         // Show replacement functionality
 //        d.replaceWhereBigger(8, -100);
